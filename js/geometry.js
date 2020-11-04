a9296509f2bd7efe9daa8fc8687d91cf67f4dd60
viewtifult <tashanduncan1994@gmail.com>
2020-11-04 20:01:25 +0000

    Completed all functions using arrow functions :D

--- js/geometry.js
+++ js/geometry.js
@@ -19,10 +19,13 @@ const getVolume = (width, height, length) => width * height * length;
 //    and return the area of that circle.
 //    The area of a circle is the value of π * radius^2
 
+const circleArea = radius => Math.PI * Math.pow(radius, 2);
+
 // 5. Create a function that calculates the volume of a sphere.
 //    The function should accept the radius of the sphere as an argument
 //    and return the volume.
 //    The volume of a circle is: 4/3 *  π * radius^3
+const sphereVolume = radius => (4 / 3) * Math.PI * Math.pow(radius, 3);
 
 // 6. Use console.log to test each function and output to the JavaScript console
 //    Here are the values to test and the expected results
@@ -30,7 +33,10 @@ const getVolume = (width, height, length) => width * height * length;
 //    -- Volume of a rectangular prism that is 4.5 x 12.5 x 17.4: 978.7499999999999
 //    -- Area of a circle that with a radius of 7.2: 162.8601631620949
 //    -- Volume of a spehere with a radius of 7.2: 1563.4575663561109
-
+console.log(getArea(5, 22))
+console.log(getVolume(4.5, 12.5, 17.4))
+console.log(circleArea(7.2))
+console.log(sphereVolume(7.2))
 
 
 
